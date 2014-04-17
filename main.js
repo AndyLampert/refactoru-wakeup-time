62949d21f8649197bd55f397c02d4617abe58a33
AndyLampert <lamperta@gmail.com>
2014-04-17 14:38:24 -0600

    added am/pm functionality

--- main.js
+++ main.js
@@ -9,13 +9,15 @@
 // Bottom FM Label & Frequencies (white)
 
 $(document).on('ready', function() {
+	// VARAIBLE DECLARATIONS
 	var outerShell = $('<div class="outer-shell"></div>');
 	var innerShell = $('<div class="inner-shell"></div>');
-	var clockScreen = $('<div class="clock-screen"><p>12:10</p></div>');
-	var clockampm = $('<div class="clock-ampm"><ul><li class="active">AM</li><li>PM</li></ul></div>');
-	var amLabel = $('<div class="am-label">content</div>');
-	var fmLabel = $('<div class="fm-label">content</div>');
+	var clockScreen = $('<div class="clock-screen"><span class="hours">12</span><span class="colon">:<span class="minutes">10</span></div>');
+	var clockampm = $('<div class="clock-ampm"><ul><li class="am-notifier">AM</li><li class="pm-notifier">PM</li></ul></div>');
+	var amLabel = $('<div class="am-label">AM 53 60 70 90 110 140 170</div>');
+	var fmLabel = $('<div class="fm-label">FM 88 92 96 102 106 108</div>');
 
+	// CREATING STRUCTURE VIA JQUER
 	$('body').append(outerShell);
 	outerShell.append(innerShell);
 	innerShell.append(clockampm);
@@ -24,8 +26,25 @@ $(document).on('ready', function() {
 	innerShell.append(fmLabel);
 
 	// when AM, add active class + remove from PM
+	var currentDate = new Date();
+	var dateHr = currentDate.getHours();
+	var dateMin = currentDate.getMinutes();
+	if(dateHr >= 12) {
+		$('.pm-notifier').addClass('active');
+		$('.am-notifier').removeClass('active');
+	} else {
+		$('.am-notifier').addClass('active');
+		$('.pm-notifier').removeClass('active');
+	}
+
 	// When PM, add active class + remove from AM
+	
 	// setinterval
+	// 
+	// Get the current time in hr/min format using the date object 
+	// Update ".clock-screen".text() or val()
+	// update .active (am/pm)
+	// target ":" ONLY
 });
 
 // Outer Shell (dark gray)
